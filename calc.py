1af34a8408c174ea0f34fa676753a6fa51a0641d
Ashwini <ashwinidukkhi@gmail.com>
2025-05-06 23:42:44 +0530

    2nd commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+num1=int(input("enter the no.1:"))
+num2=int(input("enter thr no.2:"))
+opr=input("Enter the oprater:")
+if opr=='+':
+    print(num1+num2)
+elif opr=='*':
+    print(num1*num2)
+elif opr=="/":
+    print(num1/num2)
+elif opr =="-":
+    print(num1-num2)
+else:
+    print("invalid")
+    
