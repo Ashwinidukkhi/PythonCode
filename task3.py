de5d4c951962d0c224b465d55b7c6d6c22ae493c
Ashwini <ashwinidukkhi@gmail.com>
2025-05-14 23:07:16 +0530

    Added task3.py

--- task3.py
+++ task3.py
@@ -2,7 +2,7 @@
 # Input: A positive integer. 
 # Output: "Prime" or "Not Prime".
 
-n=int(input("A positive integer"))      #  suppose n=7
+n=int(input("A positive integer : "))      #  suppose n=7
 for i in range(2,n):                    #i=2,3,4,5,6
     if n%i==0:                          #it check :7%2==0,7%3==0----7%6==0  //if remainder is =0 it is true than print no.is not prime
         print("Not prime")
